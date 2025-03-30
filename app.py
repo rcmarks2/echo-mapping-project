dd73d04a8e579e27c49dbf98e7e4f13f5e70c6b4
rcmarks2 <rcmarks2@illinois.edu>
2025-03-30 10:52:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -125,4 +125,7 @@ def result():
     return render_template("result.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
