7fdd6e32261abf95c521baee81ff1b2f60d187e3
rcmarks2 <rcmarks2@illinois.edu>
2025-04-11 10:49:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -203,3 +203,7 @@ def batch_result():
 @app.route("/download-batch")
 def download_batch():
     return send_file("static/route_results_batch.xlsx", as_attachment=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
