d543209171033f51fc53c10c1735d9528df69f61
rcmarks2 <rcmarks2@illinois.edu>
2025-04-11 22:41:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -169,7 +169,11 @@ def batch_result():
                 _, diesel_miles = get_routed_segment(start, end, return_distance=True)
 
                 diesel_total = diesel_miles * trips
-                diesel_cost = trips * (diesel_miles / mpg) * 3.59 + diesel_miles * (17500 / diesel_total) + diesel_total * (16600 / 750000)
+                annual_mileage = diesel_miles * trips
+                fuel_cost = trips * (diesel_miles / mpg) * 3.59
+                maintenance_cost = diesel_miles * (17500 / annual_mileage)
+                depreciation_cost = diesel_miles * (16600 / 750000)
+                diesel_cost = fuel_cost + maintenance_cost + depreciation_cost
                 diesel_emissions = round(diesel_total * 1.617 / 1000, 2)
 
                 if diesel_miles <= 225:
