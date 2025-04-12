f2cb9d858df2cd86ace20434dd81305bc931adab
rcmarks2 <rcmarks2@illinois.edu>
2025-04-12 10:02:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -179,6 +179,8 @@ def batch_result():
                     round(ev_cost, 2) if ev_possible == 'Yes' else 'N/A',
                     ev_emissions if ev_possible == 'Yes' else 'N/A'
                 ]
+                # Force Diesel Cost into Column 8 (H)
+                ws.cell(row=i + 3, column=8).value = round(diesel_cost, 2)
                 for col, val in enumerate(output, start=1):
                     ws.cell(row=i + 3, column=col).value = val
             except Exception as err:
