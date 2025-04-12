6dca058c7563242a85881edb74944c6fd1f6d6ea
rcmarks2 <rcmarks2@illinois.edu>
2025-04-11 22:15:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -105,8 +105,7 @@ def result():
 
         diesel_coords, diesel_miles = get_routed_segment(start, end, return_distance=True)
         diesel_total = diesel_miles * trips
-        
-                annual_mileage = diesel_miles * trips
+                        annual_mileage = diesel_miles * trips
                 fuel_cost = trips * (diesel_miles / mpg) * 3.59
                 maintenance_cost = diesel_miles * (17500 / annual_mileage)
                 depreciation_cost = diesel_miles * (16600 / 750000)
@@ -175,8 +174,7 @@ def batch_result():
                 _, diesel_miles = get_routed_segment(start, end, return_distance=True)
 
                 diesel_total = diesel_miles * trips
-                
-                annual_mileage = diesel_miles * trips
+                                annual_mileage = diesel_miles * trips
                 fuel_cost = trips * (diesel_miles / mpg) * 3.59
                 maintenance_cost = diesel_miles * (17500 / annual_mileage)
                 depreciation_cost = diesel_miles * (16600 / 750000)
