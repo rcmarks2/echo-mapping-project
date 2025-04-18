abc3c2e4c45a70dddee430a7124cff744f56a963
rcmarks2 <rcmarks2@illinois.edu>
2025-04-18 11:28:26 -0500

    instructions

--- app.py
+++ app.py
@@ -167,6 +167,7 @@ def result():
         )
     except Exception as e:
         return f"<h3>Error in single route: {e}</h3>"
+
 @app.route("/batch-result", methods=["POST"])
 def batch_result():
     try:
